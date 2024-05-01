3db01eb3adad13196b09cd21337e9c1b3a2ebec2
99mni <107456975+99mni@users.noreply.github.com>
2024-05-01 18:18:58 +0900

    [Bronze I] Title: 약수들의 합, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -0,0 +1,11 @@
+while(1):
+    N=int(input())
+    if N==-1: break
+    l=[]
+    h = 0
+    for i in range(1, N):
+        if N%i == 0:
+            l.append(i)
+    if (sum(l)==N): 
+        print(N," = "," + ".join(str(i) for i in l), sep="")
+    else: print(N, "is NOT perfect.")
