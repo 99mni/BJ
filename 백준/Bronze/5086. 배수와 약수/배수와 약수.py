97d2b0295ae35bbad866aa07794f52bdc03140ee
99mni <107456975+99mni@users.noreply.github.com>
2024-05-01 17:59:56 +0900

    [Bronze III] Title: 배수와 약수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.py
@@ -0,0 +1,9 @@
+while(1):
+    N, M = map(int, input().split())
+
+    if N==0 and M==0: 
+        break
+
+    if(N<M)and(M%N==0): print("factor")
+    elif(N>M)and(N%M==0): print("multiple")
+    else: print("neither")
