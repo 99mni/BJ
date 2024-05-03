128595480bf4bfa4a4bbc8f6472b35969d0c6412
99mni <107456975+99mni@users.noreply.github.com>
2024-05-03 14:52:07 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9032 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,11 @@
+import sys
+
+N = int(sys.stdin.readline())
+L =[0]*10001
+for _ in range(N):
+    L[int(sys.stdin.readline())] += 1
+
+for i in range(10001):
+    if L[i]!=0:
+        for _ in range(L[i]):
+            print(i)
