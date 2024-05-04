69efd627fa8d03c79b0021f985e76f6f02503fa7
99mni <107456975+99mni@users.noreply.github.com>
2024-05-04 12:51:20 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 840 ms, Memory: 114904 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,16 @@
+N = int(input())
+N_list = list(map(int,input().split()))
+M = int(input())
+M_list = list(map(int,input().split()))
+
+dic = {}
+
+for i in N_list:
+    if i in dic:
+        dic[i]+=1
+    else: dic[i]=1
+
+for i in M_list:
+    if i in dic:
+        print(dic[i], end=' ')
+    else: print(0, end=' ')
